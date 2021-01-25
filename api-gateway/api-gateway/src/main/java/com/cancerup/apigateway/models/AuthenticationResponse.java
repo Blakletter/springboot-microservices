2247e76430204b0bdd98e98e2be7469ee0b020ad
Blakletter <48567475+Blakletter@users.noreply.github.com>
2021-01-24 21:50:47 -0600

    push

--- api-gateway/api-gateway/src/main/java/com/cancerup/apigateway/models/AuthenticationResponse.java
+++ api-gateway/api-gateway/src/main/java/com/cancerup/apigateway/models/AuthenticationResponse.java
@@ -3,12 +3,9 @@ package com.cancerup.apigateway.models;
 public class AuthenticationResponse {
     private final String jwt;
     private final String name;
-
     public AuthenticationResponse(String jwt, String name) {
         this.jwt = jwt; this.name=name;
     }
-
     public String getName() {return name;}
-
     public String getJwt() {return jwt;}
 }
